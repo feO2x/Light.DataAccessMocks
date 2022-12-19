0176e25794741c36a1158d09f11561b1296d24d5
Kenny Pflug <kenny.pflug@live.de>
2022-12-19 21:35:52 +0100

    added AsyncReadOnlySessionMock

---
+++ Code/Light.DataAccessMocks.Tests/AsyncReadOnlySessionMockTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Light.SharedCore.DataAccessAbstractions;
+using Xunit;
+
+namespace Light.DataAccessMocks.Tests;
+
+public static class AsyncReadOnlySessionMockTests
+{
+    [Fact]
+    public static void MustBeAbstractClass()
+    {
+        typeof(AsyncReadOnlySessionMock<>).Should().BeAbstract();
+        typeof(AsyncReadOnlySessionMock).Should().BeAbstract();
+    }
+
+    [Fact]
+    public static void NonGenericTypeMustDeriveFromGenericType() =>
+        typeof(AsyncReadOnlySessionMock).Should().BeDerivedFrom<AsyncReadOnlySessionMock<AsyncReadOnlySessionMock>>();
+
+    [Fact]
+    public static void MustDeriveFromAsyncDisposableMock() =>
+        typeof(AsyncReadOnlySessionMock<>).Should().BeDerivedFrom(typeof(AsyncDisposableMock<>));
+
+    [Fact]
+    public static void MustImplementIAsyncReadOnlySession() =>
+        typeof(AsyncReadOnlySessionMock<>).Should().Implement<IAsyncReadOnlySession>();
+}
