138b7230ff44a6f6ab303649dcbf92bd60a5ede4
Kenny Pflug <kenny.pflug@live.de>
2022-12-19 21:32:46 +0100

    added AsyncDisposableMock

---
+++ Code/Light.DataAccessMocks.Tests/AsyncDisposableMockTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Light.DataAccessMocks.Tests;
+
+public static class AsyncDisposableMockTests
+{
+    [Fact]
+    public static void MustBeAbstractClass()
+    {
+        typeof(AsyncDisposableMock<>).Should().BeAbstract();
+        typeof(AsyncDisposableMock).Should().BeAbstract();
+    }
+
+    [Fact]
+    public static void NonGenericTypeMustDeriveFromGenericType() =>
+        typeof(AsyncDisposableMock).Should().BeDerivedFrom<AsyncDisposableMock<AsyncDisposableMock>>();
+
+    [Fact]
+    public static void MustImplementIDisposable() =>
+        typeof(AsyncDisposableMock<>).Should().Implement<IDisposable>();
+
+    [Fact]
+    public static void MustImplementIAsyncDisposable() =>
+        typeof(AsyncDisposableMock<>).Should().Implement<IAsyncDisposable>();
+
+    [Fact]
+    public static void MustImplementIDisposableMock() =>
+        typeof(AsyncDisposableMock<>).Should().Implement<IDisposableMock>();
+
+    [Fact]
+    public static void ThrowExceptionWhenNotDisposed()
+    {
+        var disposable = new AsyncDisposable();
+
+        Action act = () => disposable.MustBeDisposed();
+
+        act.Should().Throw<TestException>()
+           .And.Message.Should().Be("\"AsyncDisposable\" was not disposed.");
+    }
+
+    [Fact]
+    public static void NoExceptionWhenDisposed()
+    {
+        var disposable = new AsyncDisposable();
+
+        disposable.Dispose();
+
+        disposable.EnsureNoExceptionIsThrown();
+    }
+
+    [Fact]
+    public static async Task NoExceptionWhenDisposedAsync()
+    {
+        var disposable = new AsyncDisposable();
+
+        await disposable.DisposeAsync();
+
+        disposable.EnsureNoExceptionIsThrown();
+    }
+
+    private static void EnsureNoExceptionIsThrown(this AsyncDisposable disposable) =>
+        disposable.MustBeDisposed().Should().BeSameAs(disposable);
+
+    [Fact]
+    public static void SyncCallCountIncrementationMustBeChecked()
+    {
+        var disposable = new AsyncDisposable().SetDisposeCountToMaximum();
+
+        Action act = () => disposable.Dispose();
+
+        act.Should().Throw<OverflowException>();
+    }
+
+    [Fact]
+    public static void AsyncCallCountIncrementationMustBeChecked()
+    {
+        var disposable = new AsyncDisposable().SetDisposeCountToMaximum();
+
+        Func<Task> act = () => disposable.DisposeAsync().AsTask();
+
+        act.Should().ThrowAsync<OverflowException>();
+    }
+
+    private sealed class AsyncDisposable : AsyncDisposableMock
+    {
+        public AsyncDisposable SetDisposeCountToMaximum()
+        {
+            DisposeCallCount = int.MaxValue;
+            return this;
+        }
+    }
+}
---
+++ Code/Light.DataAccessMocks/AsyncDisposableMock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Light.DataAccessMocks;
+
+/// <summary>
+/// Represents a base class for mocks that implement <see cref="IAsyncDisposable" /> and <see cref="IDisposable" />.
+/// </summary>
+/// <typeparam name="T">
+/// The subtype that derives from this class.
+/// It is used as the return type of the fluent API.
+/// </typeparam>
+public abstract class AsyncDisposableMock<T> : DisposableMock<T>, IAsyncDisposable
+    where T : AsyncDisposableMock<T>
+{
+    /// <summary>
+    /// Increments the DisposeCallCount.
+    /// </summary>
+    public ValueTask DisposeAsync()
+    {
+        checked
+        {
+            DisposeCallCount++;
+        }
+
+        return default;
+    }
+}
+
+/// <summary>
+/// Represents a base class for mocks that implement <see cref="IAsyncDisposable" /> and <see cref="IDisposable" />.
+/// The return type of the fluent APIs is tied to this base class.
+/// </summary>
+public abstract class AsyncDisposableMock : AsyncDisposableMock<AsyncDisposableMock> { }
