4732219eef1d24db3c518e457887836de53638fb
Kenny Pflug <kenny.pflug@live.de>
2022-12-19 21:58:41 +0100

    added missing tests for AsyncTransactionSessionMock

---
+++ Code/Light.DataAccessMocks.Tests/AsyncTransactionSessionMockTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Light.DataAccessMocks.Tests;
+
+public static class AsyncTransactionalSessionMockTests
+{
+    [Fact]
+    public static void MustBeAbstractClass()
+    {
+        typeof(AsyncTransactionalSessionMock<>).Should().BeAbstract();
+        typeof(AsyncTransactionalSessionMock).Should().BeAbstract();
+    }
+
+    [Fact]
+    public static void NonGenericTypeMustDeriveFromGenericType() =>
+        typeof(AsyncTransactionalSessionMock).Should().BeDerivedFrom<AsyncTransactionalSessionMock<AsyncTransactionalSessionMock>>();
+
+    [Fact]
+    public static void MustDeriveFromBaseTransactionSessionMock() =>
+        typeof(AsyncTransactionalSessionMock).Should().BeDerivedFrom<BaseTransactionalSessionMock<AsyncTransactionMock, AsyncTransactionalSessionMock>>();
+}
